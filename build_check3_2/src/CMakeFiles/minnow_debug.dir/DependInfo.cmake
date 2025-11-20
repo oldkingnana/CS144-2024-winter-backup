9ace293867fadb4e6e2ceced688d80464bcd7b2e
oldkingnana <2472204250@qq.com>
2025-11-20 23:43:35 +0800

    在src/tcp_sender.hh新增了RetransmissionTimer类

---
+++ build_check3_2/src/CMakeFiles/minnow_debug.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/oldking/CS144-2024-winter-backup/src/byte_stream.cc" "src/CMakeFiles/minnow_debug.dir/byte_stream.cc.o" "gcc" "src/CMakeFiles/minnow_debug.dir/byte_stream.cc.o.d"
+  "/home/oldking/CS144-2024-winter-backup/src/byte_stream_helpers.cc" "src/CMakeFiles/minnow_debug.dir/byte_stream_helpers.cc.o" "gcc" "src/CMakeFiles/minnow_debug.dir/byte_stream_helpers.cc.o.d"
+  "/home/oldking/CS144-2024-winter-backup/src/reassembler.cc" "src/CMakeFiles/minnow_debug.dir/reassembler.cc.o" "gcc" "src/CMakeFiles/minnow_debug.dir/reassembler.cc.o.d"
+  "/home/oldking/CS144-2024-winter-backup/src/tcp_receiver.cc" "src/CMakeFiles/minnow_debug.dir/tcp_receiver.cc.o" "gcc" "src/CMakeFiles/minnow_debug.dir/tcp_receiver.cc.o.d"
+  "/home/oldking/CS144-2024-winter-backup/src/tcp_sender.cc" "src/CMakeFiles/minnow_debug.dir/tcp_sender.cc.o" "gcc" "src/CMakeFiles/minnow_debug.dir/tcp_sender.cc.o.d"
+  "/home/oldking/CS144-2024-winter-backup/src/wrapping_integers.cc" "src/CMakeFiles/minnow_debug.dir/wrapping_integers.cc.o" "gcc" "src/CMakeFiles/minnow_debug.dir/wrapping_integers.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -3,40 +3,55 @@
 
 using namespace std;
 
-uint64_t TCPSender::sequence_numbers_in_flight() const
+// ============== RetransmissionTimer =================
+
+bool RetransmissionTimer::is_timeout() const 
 {
-  // Your code here.
-  return {};
+	return ms_time_ > RTO_;
 }
 
-uint64_t TCPSender::consecutive_retransmissions() const
+void RetransmissionTimer::time_plus(const uint64_t& ms_since_last_tick)
 {
-  // Your code here.
-  return {};
+	ms_time_ += ms_since_last_tick;	
+}
+
+
+// =================== TCPSender ======================
+
+uint64_t TCPSender::sequence_numbers_in_flight() const // OK
+{
+  	// Your code here.
+  	return {};
+}
+
+uint64_t TCPSender::consecutive_retransmissions() const // OK
+{
+  	// Your code here.
+  	return {};
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)transmit;
-}
+	// Your code here.
+	(void)transmit;
 
-TCPSenderMessage TCPSender::make_empty_message() const
+TCPSenderMessage TCPSender::make_empty_message() const // OK
 {
-  // Your code here.
-  return {};
+  	TCPSenderMessage TCPSdMsg;
+  	TCPSdMsg.seqno = isn_;
+  	return TCPSdMsg;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  	// Your code here.
+  	(void)msg;
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
-  (void)transmit;
-  (void)initial_RTO_ms_;
+  	// Your code here.
+  	(void)ms_since_last_tick;
+  	(void)transmit;
+  	(void)initial_RTO_ms_;
 }
